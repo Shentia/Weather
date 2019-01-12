7be990f24aa21f91a1b7909133afcf5aea2795b5
Ahmadreza Shamimi <ahmadreza.shamimi@gmail.com>
2019-01-12 22:53:59 +0330

    weather

--- src/components/Weather.js
+++ src/components/Weather.js
@@ -2,7 +2,13 @@ import React, { Component } from "react";
 
 class Weather extends Component {
   render() {
-    return <div>Weather Component</div>;
+    return (
+      <div>
+        City : {this.props.city} Country:{this.props.country} Temperature :{" "}
+        {this.props.temperature} humidity: {this.props.humidity}
+        Condition: {this.props.description}
+      </div>
+    );
   }
 }
 
