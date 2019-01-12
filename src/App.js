25641a5aa3e1e4349abb07bb36c1340b72808da4
Ahmadreza Shamimi <ahmadreza.shamimi@gmail.com>
2019-01-12 17:00:27 +0330

    Add Event

--- src/App.js
+++ src/App.js
@@ -1,16 +1,27 @@
-import React from "react";
+import React, { Component } from "react";
 import Titles from "./components/Titles";
 import Form from "./components/Form";
 import Weather from "./components/Weather";
 
 const API_KEY = "7e18b308bae4df733ea42a99a4d96af7";
 
-class App extends React.Component {
+class App extends Component {
+  getWeather = async event => {
+    event.preventDefault();
+    const city = event.target.elements.city.value;
+    const country = event.target.elements.country.value;
+    const api_call = await fetch(
+      `http://api.openweathermap.org/data/2.5/weather?q=${city},${country}&appid=${API_KEY}&units=metric`
+    );
+    const data = await api_call.json();
+    console.log(data);
+  };
+
   render() {
     return (
       <div>
         <Titles />
-        <Form />
+        <Form getWeather={this.getWeather} />
         <Weather />
       </div>
     );
